c0dabdeabb128662030f1c7ec82f2333f51d1f5d
Vinh <ladykillah962@gmail.com>
2021-10-15 23:18:37 +0700

    set up base document

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Farm Organic Store</title>
+    <link rel="stylesheet" href="./assets/css/all.css" />
+    <link rel="stylesheet" href="./assets/css/base.css" />
+  </head>
+  <body>
+    <header class="header"></header>
+    <main class="main">
+      <section class="slider"></section>
+      <section class="product"></section>
+    </main>
+    <footer class="footer"></footer>
+  </body>
+</html>
