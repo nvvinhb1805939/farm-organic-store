80b699dc0cba3a9d7614f8d1baf7a246ba064357
Vinh <ladykillah962@gmail.com>
2021-10-16 07:58:01 +0700

    update header

--- index.php
+++ index.php
@@ -5,11 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Farm Organic Store</title>
-    <link rel="stylesheet" href="./assets/css/all.css" />
-    <link rel="stylesheet" href="./assets/css/base.css" />
+    <link rel="icon" href="./assets/img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./assets/css/home.css">
   </head>
   <body>
-    <header class="header"></header>
+    <header class="header">
+      <div class="header-container container flex">
+        <div class="header-logo">
+          <a href="#" class="header-link">
+            <img src="./assets/img/logo.png" alt="" class="header-link__img">
+          </a>
+        </div>
+        <nav class="header-nav">
+          <a href="#" class="header-nav__link">Trang Chủ</a>
+          <a href="#" class="header-nav__link">Sản Phẩm</a>
+          <a href="#" class="header-nav__link">Liên Hệ</a>
+        </nav>
+        <div class="header-user">
+          <button class="header-btn search">
+            <i class="far fa-search header__icon"></i>
+          </button>
+          <button class="header-btn cart">
+              <i class="far fa-shopping-cart header__icon"></i>
+          </button>
+        </div>
+      </div>
+    </header>
     <main class="main">
       <section class="slider"></section>
       <section class="product"></section>
