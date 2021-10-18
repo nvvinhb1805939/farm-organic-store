7c935d88cc3cedd02219cf8cedfcbb424e1f48bc
Vinh <ladykillah962@gmail.com>
2021-10-18 14:53:56 +0700

    update cart

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+const cart = document.querySelector(".header-cart"),
+  cartIcon = document.querySelector(".header__icon.cart"),
+  cartWrapper = document.querySelector(".header-cart__wrapper"),
+  amountLabel = document.querySelector(".header-cart__amount"),
+  totalLabel = document.querySelector(".header-cart__total"),
+  items = document.querySelectorAll(".header-cart__item"),
+  quantityItems = document.querySelectorAll(".header-cart__quantity"),
+  descreaseBtns = document.querySelectorAll(".header-cart__btn.decrease"),
+  increaseBtns = document.querySelectorAll(".header-cart__btn.increase"),
+  priceLabels = document.querySelectorAll(".header-cart__price");
+
+let amountItems = items.length;
+
+/*==============================Toggle Cart Wrapper==============================*/
+cart.onclick = e => {
+  e.stopPropagation();
+};
+cartIcon.onclick = e => {
+  cartWrapper.classList.toggle("show");
+};
+window.onclick = () => {
+  cartWrapper.classList.remove("show");
+};
+
+/*==============================Handle Quantity Items==============================*/
+descreaseBtns.forEach((descreaseBtn, index) => {
+  descreaseBtn.onclick = () => {
+    handleQuantityItems(index, false);
+  };
+});
+increaseBtns.forEach((increaseBtn, index) => {
+  increaseBtn.onclick = () => {
+    handleQuantityItems(index, true);
+  };
+});
+
+function handleQuantityItems(index, isIncrease) {
+  let quantityItem = Number(quantityItems[index].innerText);
+  let price = Number(priceLabels[index].innerText);
+  if (!isIncrease) {
+    if (quantityItem == 1) {
+      const isDel = confirm("Bạn có muốn xóa sản phẩm này khỏi giỏ hàng không?");
+      if (!isDel) {
+        return;
+      } else {
+        items[index].remove();
+        amountItems--;
+      }
+    }
+    price -= Number(items[index].dataset.price);
+    quantityItem--;
+  } else {
+    price += Number(items[index].dataset.price);
+    quantityItem++;
+  }
+  quantityItems[index].innerText = quantityItem;
+  priceLabels[index].innerText = price;
+  cart.setAttribute("data-quantity", amountItems);
+  totalLabel.innerText = getTotalPrice();
+  amountLabel.innerText = amountItems;
+}
+function getTotalPrice() {
+  let price = 0;
+  priceLabels.forEach(priceLabel => {
+    price += Number(priceLabel.innerText);
+  });
+  return price;
+}
--- index.php
+++ index.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Farm Organic Store</title>
     <link rel="icon" href="./assets/img/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="./assets/css/home.css">
+    <link rel="stylesheet" href="./assets/css/index.css">
   </head>
   <body>
     <header class="header">
-      <div class="header-container container flex">
-        <div class="header-logo">
+      <div class="header__container container flex">
+        <div class="header__logo">
           <a href="#" class="header-link">
             <img src="./assets/img/logo.png" alt="" class="header-link__img">
           </a>
@@ -21,13 +21,79 @@
           <a href="#" class="header-nav__link">Sản Phẩm</a>
           <a href="#" class="header-nav__link">Liên Hệ</a>
         </nav>
-        <div class="header-user">
-          <button class="header-btn search">
-            <i class="far fa-search header__icon"></i>
-          </button>
-          <button class="header-btn cart">
-              <i class="far fa-shopping-cart header__icon"></i>
-          </button>
+        <div class="header__user flex">
+          <form action="./search.php" class="header__form flex" method="post">
+            <input class="header__input" type="text" placeholder="Tìm kiếm sản phẩm">
+            <button class="header__btn search">
+              <i class="far fa-search header__icon search"></i>
+            </button>
+          </form>
+          <div class="header-cart" data-quantity="2">
+            <button class="header__btn cart">
+              <i class="far fa-shopping-cart header__icon cart"></i>
+            </button>
+            <div class="header-cart__wrapper">
+              <div class="header-cart__header">
+                <h5 class="header-cart__h5">
+                  <span class="header-cart__amount">2</span> 
+                  Sản Phẩm Trong Giỏ Hàng
+                </h5>
+                <h6 class="header-cart__h6">
+                  Tổng Cộng: 
+                  <span class="header-cart__total">45000</span></h6>
+              </div>
+              <ul class="heeader-cart__list">
+                <li class="header-cart__item flex" data-price="30000">
+                  <div class="header-cart__left">
+                    <a href="#" class="header-cart__link">
+                      <img src="./assets/img/product_orange.png" alt="img" class="header-cart__img">
+                    </a>
+                  </div>
+                  <div class="header-cart__right">
+                    <h6 class="header-cart__name header-cart__h6">
+                      <a href="#">Cam</a>
+                    </h6>
+                    <div class="header-cart__control flex">
+                      <button class="header-cart__btn decrease">
+                        <i class="fal fa-minus"></i>
+                      </button>
+                      <span class="header-cart__number header-cart__quantity">1</span>
+                      <button class="header-cart__btn increase">
+                        <i class="fal fa-plus"></i>
+                      </button>
+                      <span class="header-cart__number header-cart__price">30000</span>   
+                    </div>
+                  </div>
+                </li>
+                <li class="header-cart__item flex" data-price="15000">
+                  <div class="header-cart__left">
+                    <a href="#" class="header-cart__link">
+                      <img src="./assets/img/product_banana--mini.png" alt="img" class="header-cart__img">
+                    </a>
+                  </div>
+                  <div class="header-cart__right">
+                    <h6 class="header-cart__name header-cart__h6">
+                      <a href="#">Chuối</a>
+                    </h6>
+                    <div class="header-cart__control flex">
+                      <button class="header-cart__btn decrease">
+                        <i class="fal fa-minus"></i>
+                      </button>
+                      <span class="header-cart__number header-cart__quantity">1</span>
+                      <button class="header-cart__btn increase">
+                        <i class="fal fa-plus"></i>
+                      </button>
+                      <span class="header-cart__number header-cart__price">15000</span>   
+                    </div>
+                  </div>
+                </li>
+              </ul>
+              <div class="header-cart__bottom flex">
+                <a href="" class="header-cart__click view">Giỏ Hàng</a>
+                <a href="" class="header-cart__click checkout">Thanh Toán</a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </header>
@@ -36,5 +102,7 @@
       <section class="product"></section>
     </main>
     <footer class="footer"></footer>
+
+    <script src="./assets/js/main.js"></script>
   </body>
 </html>
